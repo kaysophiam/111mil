e242abf640ff796f9ac01145bcdb04a1ad5726a3
antonio <antonio@example.com>
2017-11-29 19:41:29 -0300

    cambiop ant

--- hello world/src/hello/world/HelloWorld.java
+++ hello world/src/hello/world/HelloWorld.java
@@ -8,7 +8,7 @@ package hello.world;
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("probando git con Roma");
+      System.out.println("probando git con Roma Antonio");
    }
 }
     
