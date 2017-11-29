74dcb88f6a83f62d6446166307c46cdd30d84e20
kaysophiam <amyindijones@gmail.com>
2017-11-29 19:06:45 -0300

    se agrego la clase hello world

---
+++ hello world/src/hello/world/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("probando git");
+   }
+}
+    
+
